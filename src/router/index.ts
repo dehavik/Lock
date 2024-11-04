478d05ea04bb5f760eb5f36c4320243bd24e0c1e
Benjamin De Letter <schildpad@dehavik.be>
2024-11-04 18:14:06 +0100

    use the msal plugin for vue 3 and vite

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,17 +7,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/profile',
+      name: 'profile',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: () => import('../views/ProfileView.vue'),
+    },
+  ],
 })
 
 export default router
