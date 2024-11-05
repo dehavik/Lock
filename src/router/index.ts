8a5ae26426525afadcc8b7c67ab63dfbd616a8cc
Benjamin De Letter <schildpad@dehavik.be>
2024-11-05 03:15:49 +0100

    catch unexisting paths & display not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import KeeoView from '../views/KeeoView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: KeeoView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'PageNotFound',
+      component: NotFoundView,
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <p>Requested URL: {{ $route.fullPath }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFoundView',
+}
+</script>
